a195b90df1003773ced006660e312027c881e560
d3x0r <d3x0r@github.com>
2017-04-03 13:37:53 -0700

    Fixup side tests a little

--- side0e.js
+++ side0e.js
@@ -4,17 +4,17 @@ var http = require('http');
 var port = 7734;
 var ip = '0.0.0.0';
 
+Gun.on('out', function(msg){
+	this.to.next( msg );
+	msg = JSON.stringify(msg);
+	gunPeers.forEach( function(peer){ peer.send( msg ) })
+})
+
 var gun = Gun({
 	file: 'hubdata.json',
-	s3: {
-		key: '', // AWS Access Key
-		secret: '', // AWS Secret Token
-		bucket: '' // The bucket you want to save into
-	}
 });
 
 var server = http.createServer(function(req, res){
-	// how does this care about '/gun'?
 	stream.end(); // stream
 });
 //gun.wsp(server);
@@ -31,14 +31,10 @@ wss.on('connection',acceptConnection )
 
 var gunPeers = [];  // used as a list of connected clients.
 
-Gun.on('out', function(msg){
-	msg = JSON.stringify({headers:{},body:msg});
-	gunPeers.forEach( function(peer){ peer.send( msg ) })
-})
 function acceptConnection( connection ) {
     gunPeers.push( connection );
     connection.on( 'error',function(error){console.log( "WebSocket Error:", error ) } );
-    connection.on( 'message',function(msg){gun.on('in',JSON.parse( msg).body)})
+    connection.on( 'message',function(msg){gun.on('in',JSON.parse( msg))})
     connection.on( 'close', function(reason,desc){
         // gunpeers gone.
         var i = gunPeers.findIndex( function(p){return p===connection} );
